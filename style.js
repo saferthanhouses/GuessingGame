afb2bb933938b516c0b13e617efeb0b4ff38559c
saferthanhouses <joey_oliver@hotmail.com>
2015-08-25 20:56:03 -0400

    2 second flash, win functions, hotOrNot messages

--- style.js
+++ style.js
@@ -1,3 +1,5 @@
+// TODO: Move the restart when small
+
 jQuery.fn.rotate = function(degrees) {
     $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
                  '-moz-transform' : 'rotate('+ degrees +'deg)',
@@ -19,15 +21,20 @@ function rotate() {
 	$('.arrow').rotate(rotation);
 }
 	
-$(document).ready(function(){
-	rotate();
-})
-	
 // create random number between 1-100
 var target_no = Math.floor(Math.random() * 100);
 
+
 //TODO:
 $(document).ready(function() {
+	
+	// get the initial height of the arrow in pixels
+	var arrow_height_pixels = parseInt($(".arrow").css("left"));
+	console.log(arrow_height_pixels);
+
+	console.log(getTempHeight());
+
+
 
 	// ###### set default input & clear on focus ######
 	$("input:text.guess_value").val("Enter 1-100");
@@ -36,14 +43,24 @@ $(document).ready(function() {
 	});
 	// ################################################
 
-	// on click or enter (bottom of page)
+
+	// on click or enter (bottom of page) #############
 	$("button").click(function(){
 		var guess = $("input").val(); // why does this not work with other selectors?
 		// reset the input
 		$("input").val("")
 		// validate guess
 		if (validate(guess)) {
-			alert("valid");
+			// get the height of the temp_guage
+			// move the arrow based on the height of the temp guage
+			// guess -> hot_or_not_function(guess) -> call move_arrow
+			//									   -> get hot or not words  ???
+			arrow_height_pixels = move_arrow_vert(arrow_height_pixels, 100);
+			console.log(arrow_height_pixels);
+			console.log("Target: " + target_no);
+			hotOrNot(guess);
+			// alert("valid");
+
 			// Process stuff
 		}
 		else {
@@ -54,6 +71,8 @@ $(document).ready(function() {
 			// then remove on next focus / click
 		}
 	});
+	// #################################################
+
 
 
 	// trigger click event on guess input Enter
@@ -79,6 +98,91 @@ function validate(input) {
 	}
 }
 
+function getTempHeight() {
+	return $(".temp_guage").height();
+}
+
+function hotOrNot(guess) {
+	// how to evaluate how far from the total number the guess is?
+	var difference = guess > target_no ? guess - target_no : target_no - guess;
+	if (difference===0) { return win() }
+	var message = "";
+	switch (true) {
+		case (difference<=5): 
+			message+="Burning Hot!";
+			break;
+		case (difference<=15): 
+			message+="You're Hot!";
+			break;
+		case (difference<=25): 
+			message+="You're warm.";
+			break;
+		case (difference<=35): 
+			message+="Pretty cool...";
+			break;
+		case (difference>35 && difference < 50): 
+			message+="Too cool man!";
+			break;
+		case (difference>50): 
+			message+="Ice Cold!";
+			break;
+	}
+	if (guess > target_no) { 
+		message+=" Try lower.";
+	}
+	else {
+		message+=" Go higher.";
+	}
+	flash(message);
+
+	return difference;
+
+}
+
+function move_arrow_vert(prev, number) {
+	// need to set the zero value in pixels depending on the size of the browser
+	// function to move the arrow to a height on the temp_guage
+	// from 1-100
+	// need to change this !!!! NEED THIS TO BE ABSOLUTE.
+	// e.g. setArrowHeight(value)
+	// this value comes out of a hot or not function.
+	// make it a proportion of the height of the temp_guage.
+	// how accurate do we want this to be?
+	var next_height_px = number + prev;
+	// how to: 
+	// move with jquery?
+	$(".arrow").css(
+		{"left": next_height_px + "px"}
+	);
+	return next_height_px;
+};
+
+function win() {
+	// the win animation will take a callback that will 
+	// effectively reset the page after the animation has 
+	// completed
+	winAnimation(function() {
+		location.reload();
+	});
+}
+
+function flash(message){
+	// flashes the message in the area below the Guess Button
+	// used for the hotOrNot messages and the invalid messages
+	// not used for win
+	var input = "<p class='flash'>" + message + ",</p>"
+	$("button").after(input);
+	window.setTimeout(function(){ $(".flash").remove()}, 2000);
+
+}
+
+function winAnimation(callback){
+	// TODO: fancy animation for Win
+	alert("You Win!")
+	// assume the animation takes 2 seconds,
+	// after two second reload the page
+	window.setTimeout(callback, 2000);
+}
 	
 	// validate guess
 	// if guess is good,
