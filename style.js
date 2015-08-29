6d66098fe42baf6f2015bfb6ae45773cc79df7a5
saferthanhouses <joey_oliver@hotmail.com>
2015-08-29 11:02:54 -0400

    obj-oriented complete + basic animations

--- style.js
+++ style.js
@@ -1,135 +1,22 @@
+// TODO: Fix Losing ...  
 
-// Currently -- change to a more obj-oriented method
-//			  - wrap globals in a function?
+function Game(start_lives) {
+	// game
+	this.guesses = [];
+	this.target_no = Math.round(Math.random() * 100);	// this will be different for each game, so best to keep in the instance
+	this.start_lives = start_lives;
 
-// TODO: Fix the restart
-// TODO: Make the lives go down
-// TODO: Fix temp arrow better
-// Animate moving temp arrow
-// Win Animation
-
-// function Game(){
-// 	this.guess = [];
-// 	this.initial_arrow_height = function() {
-
-// 	} 
-// }
-
-// Game.prototype.hotOrNot(){
-// 	this.initial_arrow_height 
-// }
-
-
-
-jQuery.fn.rotate = function(degrees) {
-    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
-    return $(this);
-};
-
-// setInterval(rotate, 50); // pass rotate the name of the function, not the returned value
-
-function go() {
-	console.log("GO!")
+	// graphics
+	this.initial_arrow_height = parseInt( $(".arrow").css("left") );
+	this.lives_height = parseInt( $(".lives2").css("height") );
+	this.temperature_height = parseInt( $(".temp_guage").css("height") );
+	this.lives_increment = this.lives_height / this.start_lives;
 }
 
-var rotation=0
-
-function rotate() {
-	rotation+=50;
-	$('.arrow').rotate(rotation);
-}
-
-// #################### Game Code #################
-
-var start_lives = 5;
-
-// create random number between 1-100
-var target_no = Math.round(Math.random() * 100);
-
-var guesses = [];
-
-// main script
-$(document).ready(function() {
-	var currentGame = new Game();
-	
-	// get the initial height of the arrow in pixels
-	var initial_arrow_height = parseInt($(".arrow").css("left"));
-
-	// how to get the inital height of the lives bar?
-	var initial_lives_height = parseInt($(".lives2").css("height"));
-
-	//console.log(arrow_height_pixels);
-
-	// get the height of the temperature bar
-	console.log(getTempHeight());
-
-	// get the height of the lives bar
-	console.log(getLivesHeight());
-
-	// ############# Restart Button ##################
-	$("a").on('click', function() { 
-		location.reload();
-	});
-
-	// ###### set default input & clear on focus ######
-	$("input:text.guess_value").val("Enter 1-100");
-	$("input:text.guess_value").on("focus", function() {
-		$( this ).val("");
-	});
-	// ################################################
-
-
-	// ##### on click or enter (bottom of page) #######
-	$("button").click(function(){
-
-		var guess = $("input").val(); 
-		// why does this not work with other selectors?
-		
-		// reset the input
-		$("input").val("");
-		
-		// validate guess
-		if (validate(guess)) {
-			
-			console.log("Target: " + target_no);
-
-			if (guesses.lastIndexOf(guess)==0) {
-				flash("Number Guessed Already", "yellow");
-			}
-			else 
-			{
-				guesses.push(guess);
-				hotOrNot(guess, initial_arrow_height);
-				take_lives(initial_lives_height);;
-			}
-		}
-		else 
-		{
-			flash("	Enter a number between 1 & 100!", "red");
-		}
-	});
-	// #################################################
-
-	// trigger click event on guess input Enter
-	$("input:text.guess_value").keypress(function(e) {
-		if (e.which==13) {
-			$("button").click();
-		};
-	});
-
-});
-
-
-// ################## Other functions ##############
-
-function validate(input) {
+// validate the guess 
+Game.prototype.validate = function(input) {
 	var entered = parseInt(input);
 	if ((entered <= 100) && (entered > 0)) { 
-	// will this be false in all non-number cases?
-	// NaN will always return false here
 		return true;
 	}
 	else {
@@ -137,21 +24,21 @@ function validate(input) {
 	}
 }
 
-function getTempHeight() {
-	return $(".temp_guage").height();
-}
+// hot or not does what:
+Game.prototype.hotOrNot = function(guess) {
 
-function hotOrNot(guess, arrow_height) {
-	// how to evaluate how far from the total number the guess is?
-
+	// initialize function variables
 	var message = "";
 	var color = "";
 	var arrow_height;
 
-	var difference = guess > target_no ? guess - target_no : target_no - guess;
+	// how far off is the guess?
+	var difference = guess > this.target_no ? guess - this.target_no : this.target_no - guess;
 	
-	if (difference===0) { return win(arrow_height) }
+	// is it a winning guess?
+	if (difference===0) { return this.win() } // no longer need to pass around arrow height.
 
+	// assign variables depending on the difference
 	switch (true) {
 		case (difference<=5): 
 			message+="Burning Hot!";
@@ -185,87 +72,157 @@ function hotOrNot(guess, arrow_height) {
 			break;
 	}
 
-	if (guess > target_no) { 
+	// direction to go in
+	if (guess > this.target_no) { 
 		message+=" Try lower...";
 	}
 	else {
 		message+=" Go higher...";
 	};
 
-	// flash the message up.
-	// How can we add a css selector 
-	flash(message, color);
+	this.move_arrow(arrow_height);
 
-	// move arrow to position
-	move_arrow_vert(arrow_height, arrow_height);
-
-	return difference;
+	// Don't show the message if out of lives
+	if (this.guesses.length == this.start_lives){
+		return;
+	}
+	// flash the message up w appropriate color
+	this.flash(message, color);
 
 }
 
-function move_arrow_vert(arrow_h, init_height) {
-	// given that the arrow always start in the right spot.
-	// what displacement do we have to give it?
-
-	var position = getTempHeight();
-	// left =
-	var height = position * arrow_h;
-
-	$(".arrow").css(
-		{"left": height + init_height + "px"}
-	);
-
-};
-
-function win(init_height) {
-	//TODO: Why is this lower than the other '1's	
-	move_arrow_vert(1, init_height);
-
-	// the win animation will take a callback that will 
-	// effectively reset the page after the animation has 
-	// completed
-	winAnimation(function() {
-		location.reload();
-	});
-};
-
-function flash(message, color){
-	// flashes the message in the area below the Guess Button
-	// used for the hotOrNot messages and the invalid messages
-	// not used for win
+// Flashes message to the screen in given color.
+Game.prototype.flash = function(message, color) {
+	
+	// string to insert
 	var input = "<p class='flash'>" + message + "</p>";
+	// if no color then black
 	color = color ? color : "black"; 
+	
+	// insert, hide, then fadeIn to opaque
 	$("button").after(input);
 	$(".flash").css("color", color );
 	$(".flash").hide().fadeIn();
 
-	// let's get this to fade in and out.
+	// fadeOut then remove 
 	window.setTimeout(function(){
 		$(".flash").fadeOut(400, function() { 
 			$( this ).remove();
 		});
 	}, 400);
-
 };
 
-function winAnimation(callback){
-	// TODO: fancy animation for Win
+// move the arrow to the arrow_height proportion of the height of the bar
+Game.prototype.move_arrow = function(arrow_height) {
+	// new height of the arrow (proportion of bar) * 
+	var new_height = arrow_height * this.temperature_height;
+
+	// move the arrow to that height by adding the pixels to the arrow's original position
+	$(".arrow").animate(
+		{"left": new_height + this.initial_arrow_height + "px"}
+	);
+	// hmmmm. should be able to fix this positioning issue
+	// at it's source...
+};
+
+// change height of lives bar depending how many guesses used
+Game.prototype.take_lives = function() {	
+	var new_height_str = this.lives_increment * (this.start_lives - this.guesses.length) + "px";
+	$(".lives2").animate({"height": new_height_str});
+	if (this.guesses.length == this.start_lives){
+		this.lose();
+	}
+};
+
+
+// win animation handler
+Game.prototype.win = function() {
+	this.winAnimation(function() {
+		location.reload();
+	});
+};
+
+// TODO: crazy animation goes here :)
+Game.prototype.winAnimation = function(callback){
 	alert("You Win!")
-	// assume the animation takes 2 seconds,
-	// after two second reload the page
+	this.move_arrow(1);
 	window.setTimeout(callback, 2000);
 };
 
-function take_lives(lives_height) {
-	// changes the height of the lives bar depending on
-	// how many lives are remaining.
-	var each_life_height = lives_height/start_lives;
-	var new_height = each_life_height * (start_lives - guesses.length) + "px";
-	console.log("lives height:" + new_height);
-	// why is the css assignment not working?
-	$("lives2").css("height", new_height);
+// lose animation handler
+Game.prototype.lose = function() {
+
+	this.loseAnimation(function() {
+		location.reload();
+	});
 };
 
-function getLivesHeight() {
-	return $(".lives2").height();	
+// TODO: crazy animation goes here :)
+Game.prototype.loseAnimation = function(callback) {
+	alert("you lose sucker!")
+	window.setTimeout(callback, 2000);
 };
+// ##############################################
+
+
+
+// main script
+$(document).ready(function() {
+
+	// initalize instance of current game
+	var currentGame = new Game(5);
+
+	// print target for testing 
+	console.log(currentGame.target_no);
+
+	// Restart Button
+	$("a").on('click', function() { 
+		location.reload();
+	});
+
+	// Defaut input text and clear on focus
+	$("input:text.guess_value").val("Enter 1-100"); 
+	$("input:text.guess_value").on("focus", function() {
+		$( this ).val("");
+	});
+
+	// trigger click event on 'Enter' to input guess
+	$("input:text.guess_value").keypress(function(e) {
+		if (e.which==13) {
+			$("button").click();
+		};
+	});
+
+	//  When 'Guess' button is clicked (or on 'Enter')
+	$("button").click(function(){
+
+		var guess = $("input").val(); 
+		
+		// reset the input
+		$("input").val("");
+		
+		// validate guess
+		if (currentGame.validate(guess)) 
+		{
+			
+			// console.log("Target: " + target_no);
+
+			// check it's not in guesses
+			if (currentGame.guesses.lastIndexOf(guess)==0) 
+			{
+				currentGame.flash("Number Guessed Already", "yellow");
+			}
+			else 
+			{
+				currentGame.guesses.push(guess);
+				currentGame.hotOrNot(guess);
+				currentGame.take_lives();
+			};
+		}
+		else 
+		{
+			currentGame.flash("	Enter a number between 1 & 100!", "red");
+		}
+	});
+ });
+
